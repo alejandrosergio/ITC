328a780a4117add6e438acc25a3969844f1cbeee
ALEJANDRO seeik <alejandroseeik@MacBook-Air-de-Sergiio.local>
2021-11-15 17:22:05 -0500

    80/100

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,24 @@
 
 $(document).ready(function(){
 
+    // navigation bar toggle
     $('#navbar-toggler').click(function(){
         $('.navbar-collapse').slideToggle(400);
+    });
+
+    $('.nav-item').click(function(){
+        $('.navbar-collapse').slideToggle(0);
+    });
+
+    // navbar bg change on scroll
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        }else{
+            $('.navbar').removeClass('cng-navbar');
+        }
     });
 
 });
